99e079abfcbf6406a5d67494533043f3ae452772
LuizMartendal <luiz.martendal52@gmail.com>
2024-01-10 08:31:56 -0300

    user service and repository implementeds

---
+++ authuser/src/main/java/com/ead/authuser/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.ead.authuser.repositories;
+
+import com.ead.authuser.models.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<UserModel, UUID> {
+}
