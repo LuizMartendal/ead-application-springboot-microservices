5d5feb2fd6addf457aab8aa8de0cd4049d5fd80f
LuizMartendal <luiz.martendal52@gmail.com>
2024-01-12 16:37:21 -0300

    all endpoints mapped

---
+++ course/src/main/java/com/ead/course/controllers/ModuleController.java
@@ -0,0 +1,90 @@
+package com.ead.course.controllers;
+
+import com.ead.course.dtos.ModuleDto;
+import com.ead.course.models.CourseModel;
+import com.ead.course.models.ModuleModel;
+import com.ead.course.services.CourseService;
+import com.ead.course.services.ModuleService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+public class ModuleController {
+
+    @Autowired
+    ModuleService moduleService;
+
+    @Autowired
+    CourseService courseService;
+
+    @PostMapping("/courses/{courseId}/modules")
+    public ResponseEntity<Object> saveModule(
+            @RequestBody @Valid ModuleDto moduleDto,
+            @PathVariable UUID courseId
+    ) {
+        Optional<CourseModel> courseModelOptional = courseService.findById(courseId);
+        if (courseModelOptional.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Course not found!");
+        }
+        ModuleModel moduleModel = new ModuleModel();
+        BeanUtils.copyProperties(moduleDto, moduleModel);
+        moduleModel.setCourse(courseModelOptional.get());
+        moduleModel.setCreationDate(LocalDateTime.now(ZoneId.of("UTC")));
+        return ResponseEntity.status(HttpStatus.CREATED).body(moduleService.save(moduleModel));
+    }
+
+    @DeleteMapping("/courses/{courseId}/modules/{moduleId}")
+    public ResponseEntity<Object> deleteModule(
+            @PathVariable UUID courseId, @PathVariable UUID moduleId
+    ) {
+        Optional<ModuleModel> moduleModelOptional = moduleService.findModuleIntoCourse(courseId, moduleId);
+        if (moduleModelOptional.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Module not found for this course");
+        }
+        moduleService.delete(moduleModelOptional.get());
+        return ResponseEntity.status(HttpStatus.OK).body("Module deleted successfully");
+    }
+
+    @PutMapping("/courses/{courseId}/modules/{moduleId}")
+    public ResponseEntity<Object> updateModule(
+            @PathVariable UUID courseId,
+            @PathVariable UUID moduleId,
+            @RequestBody @Valid ModuleDto moduleDto
+    ) {
+        Optional<ModuleModel> moduleModelOptional = moduleService.findModuleIntoCourse(courseId, moduleId);
+        if (moduleModelOptional.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Module not found for this course");
+        }
+        ModuleModel moduleModel = moduleModelOptional.get();
+        moduleModel.setTitle(moduleDto.getTitle());
+        moduleModel.setDescription(moduleDto.getDescription());
+        return ResponseEntity.status(HttpStatus.OK).body(moduleService.save(moduleModel));
+    }
+
+    @GetMapping("/courses/{courseId}/modules")
+    public ResponseEntity<List<ModuleModel>> getAllModules(@PathVariable UUID courseId) {
+        return ResponseEntity.status(HttpStatus.OK).body(moduleService.findAllByCourse(courseId));
+    }
+
+    @GetMapping("/courses/{courseId}/modules/{moduleId}")
+    public ResponseEntity<Object> getOneModule(
+            @PathVariable UUID courseId, @PathVariable UUID moduleId
+    ) {
+        Optional<ModuleModel> moduleModelOptional = moduleService.findModuleIntoCourse(courseId, moduleId);
+        if (moduleModelOptional.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Course not found!");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(moduleModelOptional.get());
+    }
+}
