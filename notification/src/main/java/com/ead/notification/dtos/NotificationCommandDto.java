3e170cf72b1b2893950e9cf76dd468a77a11a2c7
LuizMartendal <luiz.martendal52@gmail.com>
2024-02-14 18:36:38 -0300

    update

---
+++ notification/src/main/java/com/ead/notification/dtos/NotificationCommandDto.java
@@ -0,0 +1,11 @@
+package com.ead.notification.dtos;
+
+import java.util.UUID;
+
+public class NotificationCommandDto {
+
+    private String title;
+    private String message;
+    private UUID userId;
+
+}
