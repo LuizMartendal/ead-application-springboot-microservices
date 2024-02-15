88f2f926c00c4cdca73e87d77253027822795c77
LuizMartendal <luiz.martendal52@gmail.com>
2024-02-15 16:07:02 -0300

    amqp config and notification save method implemented

--- notification/src/main/java/com/ead/notification/dtos/NotificationCommandDto.java
+++ notification/src/main/java/com/ead/notification/dtos/NotificationCommandDto.java
@@ -8,4 +8,28 @@ public class NotificationCommandDto {
     private String message;
     private UUID userId;
 
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public void setUserId(UUID userId) {
+        this.userId = userId;
+    }
 }
